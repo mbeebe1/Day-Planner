ad2c4ec6e2498657f5eede5ec749fddb2be5130d
Michael Beebe <beebe79@gmail.com>
2021-02-24 17:29:03 -0500

    added date and time

---
+++ script.js
@@ -0,0 +1,14 @@
+var time = moment().format('MMMM Do YYYY, h:mm a');
+$('#currentDay').text(time);
+
+var firstHour = $('#9am');
+var secondHour = $('#10am');
+var thirdHour = $('#11am');
+var fourthHour = $('#12pm');
+var fifthHour = $('#1pm');
+var sixthHour = $('#2pm');
+var seventhHour = $('#3pm');
+var eighthHour = $('#4pm');
+var ninthHour = $('#5pm');
+
+
