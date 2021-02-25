8efe6022d00f5d64b89a58ff3b2aa5d70b5ccf87
Michael Beebe <beebe79@gmail.com>
2021-02-24 19:46:27 -0500

    html changes

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-var time = moment().format('MMMM Do YYYY, h:mm a');
+// set date and time
+var time = moment().format('MMMM Do YYYY');
 $('#currentDay').text(time);
 
-var firstHour = $('#9am');
-var secondHour = $('#10am');
-var thirdHour = $('#11am');
-var fourthHour = $('#12pm');
-var fifthHour = $('#1pm');
-var sixthHour = $('#2pm');
-var seventhHour = $('#3pm');
-var eighthHour = $('#4pm');
-var ninthHour = $('#5pm');
 
 
