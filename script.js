8d4362214651b3d7531d546d29c3d96cb5f92673
Michael Beebe <beebe79@gmail.com>
2021-02-26 17:46:54 -0500

    changed html to military time, got colors to work, but need to specify the hours more

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 // set date and time
-var time = moment().format('MMMM Do YYYY');
-$('#currentDay').text(time);
-
+var time = moment().format("MMMM Do YYYY");
+$("#currentDay").text(time);
 
 var saveButton = $(".saveBtn");
 // save appointments to local storage with click function
-saveButton.on("click", function(){
-    var appt = $(this).siblings('textarea').val();
-    var storage = $(this).parent().attr('id');
-    localStorage.setItem(storage, JSON.stringify(appt));
+saveButton.on("click", function () {
+  var appt = $(this).siblings("textarea").val();
+  var storage = $(this).parent().attr("id");
+  localStorage.setItem(storage, JSON.stringify(appt));
 });
 // get data from local storage when user comes back
-$('#9AM textarea').val(JSON.parse(localStorage.getItem("9AM")));
-$('#10AM textarea').val(JSON.parse(localStorage.getItem("10AM")));
-$('#11AM textarea').val(JSON.parse(localStorage.getItem("11AM")));
-$('#12PM textarea').val(JSON.parse(localStorage.getItem("12PM")));
-$('#1PM textarea').val(JSON.parse(localStorage.getItem("1PM")));
-$('#2PM textarea').val(JSON.parse(localStorage.getItem("2PM")));
-$('#3PM textarea').val(JSON.parse(localStorage.getItem("3PM")));
-$('#4PM textarea').val(JSON.parse(localStorage.getItem("4PM")));
-$('#5PM textarea').val(JSON.parse(localStorage.getItem("5PM")));
+$("#9 textarea").val(JSON.parse(localStorage.getItem("9")));
+$("#10 textarea").val(JSON.parse(localStorage.getItem("10")));
+$("#11 textarea").val(JSON.parse(localStorage.getItem("11")));
+$("#12 textarea").val(JSON.parse(localStorage.getItem("12")));
+$("#13 textarea").val(JSON.parse(localStorage.getItem("13")));
+$("#14 textarea").val(JSON.parse(localStorage.getItem("14")));
+$("#15 textarea").val(JSON.parse(localStorage.getItem("15")));
+$("#16 textarea").val(JSON.parse(localStorage.getItem("16")));
+$("#17 textarea").val(JSON.parse(localStorage.getItem("17")));
 // function to color code rows during time ranges
-var currentHour = parseInt(moment().format('hh'));
-$('.col-md-10').each(function(){
-    var val = parseInt($(this).attr('id'));
-    if (val === currentHour){
-        $(this).css('.present');
-    }else if (val > currentHour && val < currentHour+6){
-        $(this).css('.future');
-    }else if (val < currentHour && val > currentHour-6){
-        $(this).css('.past');
-    }else {
-        $(this).css('background-color','white');
-    }
-});
 
+var currentHour = moment().format("H");
+$(".col-md-10").each(function () {
+  var val = parseInt($(this).attr("id"));
+  if (val > 9) {
+    $(this).css("background-color", "lightgray");
+  } else if (val < currentHour && val > currentHour - 6) {
+    $(this).css("background-color", "Green");
+  } else if (val === currentHour) {
+    $(this).css("background-color", "Red");
+  }
+});
