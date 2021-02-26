42f9a3e40593752921fb5709bd3a5e94485c9cc4
Michael Beebe <beebe79@gmail.com>
2021-02-26 14:19:25 -0500

    added function to save appts to local storage

--- script.js
+++ script.js
@@ -2,5 +2,11 @@
 var time = moment().format('MMMM Do YYYY');
 $('#currentDay').text(time);
 
-
+var saveButton = $(".saveBtn");
+// save appointments to local storage with click function
+saveButton.on("click", function(){
+    var appt = $(this).siblings('textarea').val();
+    var storage = $(this).parent().attr('id');
+    localStorage.setItem(storage, JSON.stringify(appt));
+});
 
